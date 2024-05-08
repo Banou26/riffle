352cdb3277e988bc3cc44c485787acd3bbc16766
Banou <me@banou.dev>
2024-05-08 17:57:41 -0400

    feat: ✨tracker scrape

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
+#[macro_use]
+extern crate serde_derive;
+
+use urlencoding::{encode_binary};
+use anyhow::Result;
+
 mod meta_info;
 mod tracker;
 mod utils;
 
 use meta_info::{read_meta_info_file, render_meta_info, info_hash_hex, info_hash_buffer};
-use tracker::{announce_buffer};
+use tracker::{fetch_announce_buffer_to_struct, fetch_announce};
 use utils::{read_file};
 
-use urlencoding::{encode_binary};
-
-#[macro_use]
-extern crate serde_derive;
-
-
-
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let meta_info: meta_info::MetaInfo = read_meta_info_file("./sintel.torrent").unwrap();
     let info_hash = info_hash_hex(&meta_info);
     let info_hash_buffer = info_hash_buffer(&meta_info).unwrap();
@@ -24,12 +23,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let result = encode_binary(&info_hash_buffer);
     println!("infohash uri: {}", result);
 
-    let announce_url = "http://tracker.opentrackr.org:1337/announce?info_hash=%08%AD%A5%A7%A6%18%3A%AE%1E%09%D81%DFgH%D5f%09Z%10";
-    let resp = announce_buffer(announce_url).await?;
+    // let scrape_url = "http://tracker.opentrackr.org:1337/scrape?info_hash=%08%AD%A5%A7%A6%18%3A%AE%1E%09%D81%DFgH%D5f%09Z%10";
+    // let scrape_response = tracker::fetch_scrape(scrape_url).await?;
+
+    let resp = read_file("./scrape").unwrap();
+    let scrape_response = tracker::fetch_scrape_buffer_to_struct(resp).unwrap();
+    println!("{:?}", scrape_response);
+
+
+    // let annnounce_url = "http://tracker.opentrackr.org:1337/announce?info_hash=%08%AD%A5%A7%A6%18%3A%AE%1E%09%D81%DFgH%D5f%09Z%10";
+    // let announce_response = fetch_announce(annnounce_url).await?;
+
     // let resp = read_file("./announce").unwrap();
-    
-    let tracker_response = tracker::announce_response_to_struct(resp).unwrap();
-    println!("{:?}", tracker_response);
+    // let announce_response = announce::fetch_announce_buffer_to_struct(resp).unwrap();
+    // println!("{:?}", announce_response);
 
     Ok(())
 }
--- src/tracker.rs
+++ src/tracker.rs
@@ -1,8 +1,7 @@
+use std::{collections::BTreeMap, net::{Ipv4Addr, Ipv6Addr}};
+
 use anyhow::{Context, Result};
 use serde_bytes::ByteBuf;
-use std::net::{Ipv4Addr, Ipv6Addr};
-
-pub struct StringIpAddr(pub String);
 
 #[derive(Debug, Deserialize, Serialize)]
 pub enum IpAddr {
@@ -12,7 +11,7 @@ pub enum IpAddr {
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct PeerStruct {
+pub struct Peer {
     #[serde(default)]
     #[serde(rename = "peer id")]
     pub peer_id: Option<String>,
@@ -24,12 +23,12 @@ pub struct PeerStruct {
 #[serde(untagged)]
 pub enum Peers {
   ByteBuf(ByteBuf),
-  PeerStruct(Vec<PeerStruct>)
+  PeerStruct(Vec<Peer>)
 }
 
 #[allow(dead_code)]
 #[derive(Debug, Deserialize, Serialize)]
-pub struct TrackerResponse {
+pub struct TrackerAnnounceResponse {
     pub complete: i64,
     #[serde(default)]
     pub downloaded: Option<i64>,
@@ -50,11 +49,35 @@ pub struct TrackerResponse {
     pub warning_message: Option<String>
 }
 
-// announce url example sintel.torrent
-// http://tracker.opentrackr.org:1337/announce?info_hash=%08%AD%A5%A7%A6%18%3A%AE%1E%09%D81%DFgH%D5f%09Z%10
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerScrapeFile {
+  complete: i64,
+  downloaded: i64,
+  incomplete: i64,
+  #[serde(default)]
+  name: Option<String>
+}
 
-pub async fn announce_buffer(url: &str) -> Result<Vec<u8>> {
-  let resp = reqwest::get(url)
+#[allow(dead_code)]
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerScrapeResponse {
+  files: BTreeMap<ByteBuf, TrackerScrapeFile>,
+
+  #[serde(default)]
+  #[serde(rename = "failure reason")]
+  failure_reason: Option<String>,
+
+  #[serde(default)]
+  #[serde(rename = "warning message")]
+  warning_message: Option<String>,
+}
+
+/**
+ * Fetches the announce response from the tracker as buffer.
+ * announce url example sintel.torrent: http://tracker.opentrackr.org:1337/scrape?info_hash=%08%AD%A5%A7%A6%18%3A%AE%1E%09%D81%DFgH%D5f%09Z%10
+ */
+pub async fn fetch_scrape_buffer(url: &str) -> Result<Vec<u8>> {
+  let resp: Vec<u8> = reqwest::get(url)
     .await?
     .bytes()
     .await?
@@ -63,7 +86,39 @@ pub async fn announce_buffer(url: &str) -> Result<Vec<u8>> {
   Ok(resp)
 }
 
-pub fn announce_response_to_struct(response: Vec<u8>) -> Result<TrackerResponse> {
+pub fn fetch_scrape_buffer_to_struct(response: Vec<u8>) -> Result<TrackerScrapeResponse> {
+  let response_struct = serde_bencode::from_bytes(&response)
+    .with_context(|| {
+      let str_resp = String::from_utf8_lossy(&response);
+      format!("Bad tracker response {}", str_resp)
+    });
+  
+  Ok(response_struct?)
+}
+
+pub async fn fetch_scrape(url: &str) -> Result<TrackerScrapeResponse> {
+  let resp: Vec<u8> =
+    fetch_scrape_buffer(url)
+    .await
+    .context("Failed to fetch scrape")?;
+  fetch_scrape_buffer_to_struct(resp)
+}
+
+/**
+ * Fetches the announce response from the tracker as buffer.
+ * announce url example sintel.torrent: http://tracker.opentrackr.org:1337/announce?info_hash=%08%AD%A5%A7%A6%18%3A%AE%1E%09%D81%DFgH%D5f%09Z%10
+ */
+pub async fn fetch_announce_buffer(url: &str) -> Result<Vec<u8>> {
+  let resp: Vec<u8> = reqwest::get(url)
+    .await?
+    .bytes()
+    .await?
+    .to_vec();
+
+  Ok(resp)
+}
+
+pub fn fetch_announce_buffer_to_struct(response: Vec<u8>) -> Result<TrackerAnnounceResponse> {
   let response_struct = serde_bencode::from_bytes(&response)
     .with_context(|| {
       let str_resp = String::from_utf8_lossy(&response);
@@ -71,7 +126,7 @@ pub fn announce_response_to_struct(response: Vec<u8>) -> Result<TrackerResponse>
     });
   
   let normalized_response = {
-    let mut response_struct: TrackerResponse = response_struct?;
+    let mut response_struct: TrackerAnnounceResponse = response_struct?;
     match response_struct.peers {
       Peers::ByteBuf(peers) => {
         let peers =
@@ -81,7 +136,7 @@ pub fn announce_response_to_struct(response: Vec<u8>) -> Result<TrackerResponse>
             .map(|chunk| {
               let ip = Ipv4Addr::new(chunk[0], chunk[1], chunk[2], chunk[3]);
               let port  = ((chunk[4] as u16) << 8) | chunk[5] as u16;
-              PeerStruct {
+              Peer {
                 peer_id: None,
                 ip: IpAddr::V4(ip),
                 port
@@ -96,4 +151,13 @@ pub fn announce_response_to_struct(response: Vec<u8>) -> Result<TrackerResponse>
   };
 
   Ok(normalized_response)
+}
+
+
+pub async fn fetch_announce(url: &str) -> Result<TrackerAnnounceResponse> {
+  let resp: Vec<u8> =
+    fetch_announce_buffer(url)
+    .await
+    .context("Failed to fetch announce")?;
+  fetch_announce_buffer_to_struct(resp)
 }
