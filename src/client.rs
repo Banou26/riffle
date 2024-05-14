5eb798400c4caa6ac25d98cf77f922e9ad46d33f
Banou <me@banou.dev>
2024-05-13 23:08:25 -0400

    feat: ✨setup TorrentClient & tui state display

--- src/client.rs
+++ src/client.rs
@@ -1,40 +1,112 @@
-use crossbeam_channel::Receiver;
-use futures_signals::signal_map::{MutableBTreeMap, MutableSignalMap};
+use std::clone;
+
+use anyhow::{Context, Result};
+use crossbeam_channel::{unbounded, Receiver, Sender};
+use futures_signals::signal::{Mutable};
+use futures_signals::signal_map::{MapDiff, MutableBTreeMap};
 
 use crate::meta_info::{info_hash_hex, MetaInfo};
 use crate::torrent::Torrent;
 
-pub struct AddTorrent {
+pub struct AddTorrentOptions {
     pub meta_info: MetaInfo,
     pub response_tx: crossbeam_channel::Sender<()>,
 }
 
 pub enum ClientMessage {
-    AddTorrent(AddTorrent),
+    AddTorrent(AddTorrentOptions),
     RemoveTorrent(String)
 }
 
-pub fn make_client(rx: Receiver<ClientMessage>) -> MutableSignalMap<String, Torrent>{
-    let torrents: MutableBTreeMap<String, Torrent> = MutableBTreeMap::new();
-    let mut lock = torrents.lock_mut();
-
-    rx.iter().for_each(|message| {
-        match message {
-            ClientMessage::AddTorrent(add_torrent) => {
-                let info_hash = info_hash_hex(&add_torrent.meta_info);
-                let torrent = Torrent {
-                    info_hash: info_hash.clone(),
-                    meta_info: add_torrent.meta_info,
-                    inserted_at: std::time::Instant::now(),
-                };
-                lock.clone().insert(info_hash, torrent);
-                add_torrent.response_tx.send(()).unwrap();
-            },
-            ClientMessage::RemoveTorrent(info_hash) => {
-                lock.clone().remove(&info_hash);
-            }
-        }
-    });
-
-    torrents.signal_map_cloned()
+#[derive(Debug, Clone)]
+pub struct TorrentClient {
+    tx: Sender<ClientMessage>,
+    rx: Receiver<ClientMessage>,
+    // pub torrents: Vec<Torrent>,
+    pub torrents_mut: MutableBTreeMap<String, Torrent>,
 }
+
+impl TorrentClient {
+    pub fn new() -> Self {
+        let (tx, rx) = unbounded::<ClientMessage>();
+        let torrents_mut: MutableBTreeMap<String, Torrent> = MutableBTreeMap::new();
+
+        let cloned_torrents_mut = torrents_mut.clone();
+        let cloned_rx = rx.clone();
+        tokio::spawn(async move {
+            cloned_rx.iter().for_each(|message| {
+                let lock = cloned_torrents_mut.lock_mut();
+                match message {
+                    ClientMessage::AddTorrent(options) => {
+                        let info_hash = info_hash_hex(&options.meta_info);
+                        let torrent = Torrent {
+                            info_hash: info_hash.clone(),
+                            meta_info: options.meta_info,
+                            inserted_at: std::time::Instant::now(),
+                        };
+                        lock.clone().insert(info_hash, torrent);
+                        options.response_tx.send(()).unwrap();
+                    },
+                    ClientMessage::RemoveTorrent(info_hash) => {
+                        lock.clone().remove(&info_hash);
+                    }
+                }
+            })
+        });
+
+        Self {
+            tx,
+            rx,
+            torrents_mut
+        }
+    }
+
+    // pub async fn add_torrent(&self, meta_info: MetaInfo) -> Result<()>{
+    //     let (tx, rx) = unbounded::<()>();
+    //     self.tx.send(ClientMessage::AddTorrent(AddTorrentOptions {
+    //         meta_info,
+    //         response_tx: tx,
+    //     })).context("Failed to send add torrent message")?;
+    //     rx.recv().context("Failed to add torrent")?;
+    //     Ok(())
+    // }
+
+    // pub fn remove_torrent(&self, info_hash: String) {
+    //     self.torrents.lock_mut().remove(&info_hash);
+    // }
+}
+
+// pub fn make_client(rx: Receiver<ClientMessage>) -> Mutable<TorrentClient> {
+//     let torrents: MutableBTreeMap<String, Torrent> = MutableBTreeMap::new();
+//     let client = Mutable::new(TorrentClient {
+//         torrents: torrents.clone(),
+//     });
+
+//     tokio::spawn(async move {
+//         rx.iter().for_each(|message| {
+//             let lock = torrents.lock_mut();
+//             match message {
+//                 ClientMessage::AddTorrent(add_torrent) => {
+//                     let info_hash = info_hash_hex(&add_torrent.meta_info);
+//                     let torrent = Torrent {
+//                         info_hash: info_hash.clone(),
+//                         meta_info: add_torrent.meta_info,
+//                         inserted_at: std::time::Instant::now(),
+//                     };
+//                     lock.clone().insert(info_hash, torrent);
+//                     add_torrent.response_tx.send(()).unwrap();
+//                 },
+//                 ClientMessage::RemoveTorrent(info_hash) => {
+//                     lock.clone().remove(&info_hash);
+//                 }
+//             }
+//         })
+//     });
+
+//     client
+// }
+
+// pub fn add_torrent(client: &Mutable<TorrentClient>, add_torrent: AddTorrent) {
+//     let message = ClientMessage::AddTorrent(add_torrent);
+//     client.lock_mut().torrents.lock_mut().signal(message);
+// }
--- src/tui.rs
+++ src/tui.rs
@@ -1,21 +1,162 @@
-use std::io::{self, stdout, Stdout};
+use std::time::Duration;
 
-use crossterm::{execute, terminal::*};
-use ratatui::prelude::*;
+use anyhow::Result;
+use crossbeam_channel::unbounded;
+use futures_signals::signal::SignalExt;
+use ratatui::{prelude::*, widgets::*};
+use tokio::sync::mpsc;
 
-/// A type alias for the terminal type used in this application
-pub type Tui = Terminal<CrosstermBackend<Stdout>>;
+use crate::client::{self, ClientMessage, TorrentClient};
 
-/// Initialize the terminal
-pub fn init() -> io::Result<Tui> {
-    execute!(stdout(), EnterAlternateScreen)?;
-    enable_raw_mode()?;
-    Terminal::new(CrosstermBackend::new(stdout()))
+pub fn initialize_panic_handler() {
+  let original_hook = std::panic::take_hook();
+  std::panic::set_hook(Box::new(move |panic_info| {
+    shutdown().unwrap();
+    original_hook(panic_info);
+  }));
 }
 
-/// Restore the terminal to its original state
-pub fn restore() -> io::Result<()> {
-    execute!(stdout(), LeaveAlternateScreen)?;
-    disable_raw_mode()?;
-    Ok(())
+pub fn startup() -> Result<()> {
+  crossterm::terminal::enable_raw_mode()?;
+  crossterm::execute!(std::io::stderr(), crossterm::terminal::EnterAlternateScreen)?;
+  Ok(())
+}
+
+pub fn shutdown() -> Result<()> {
+  crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen)?;
+  crossterm::terminal::disable_raw_mode()?;
+  Ok(())
+}
+
+pub struct App {
+  action_tx: mpsc::UnboundedSender<Action>,
+  counter: i64,
+  should_quit: bool,
+  ticker: i64,
+  torrent_hashes: Vec<String>,
+}
+
+pub fn ui(f: &mut Frame, app: &mut App) {
+  let area = f.size();
+  f.render_widget(
+    Paragraph::new(format!(
+      "Press j or k to increment or decrement.\n\nCounter: {}\n\nTicker: {}\n\nTorrents: {:?}",
+      app.counter, app.ticker, app.torrent_hashes
+    ))
+    .block(
+      Block::default()
+        .title("ratatui async counter app")
+        .title_alignment(Alignment::Center)
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded),
+    )
+    .style(Style::default().fg(Color::Cyan))
+    .alignment(Alignment::Center),
+    area,
+  );
+}
+
+#[derive(PartialEq)]
+pub enum Action {
+  ScheduleIncrement,
+  ScheduleDecrement,
+  Increment,
+  Decrement,
+  Quit,
+  None,
+}
+
+pub fn update(app: &mut App, msg: Action) -> Action {
+  match msg {
+    Action::Increment => {
+      app.counter += 1;
+    },
+    Action::Decrement => {
+      app.counter -= 1;
+    },
+    Action::ScheduleIncrement => {
+      let tx = app.action_tx.clone();
+      tokio::spawn(async move {
+        // tokio::time::sleep(Duration::from_secs(5)).await;
+        tx.send(Action::Increment).unwrap();
+      });
+    },
+    Action::ScheduleDecrement => {
+      let tx = app.action_tx.clone();
+      tokio::spawn(async move {
+        // tokio::time::sleep(Duration::from_secs(5)).await;
+        tx.send(Action::Decrement).unwrap();
+      });
+    },
+    Action::Quit => app.should_quit = true, // You can handle cleanup and exit here
+    _ => {},
+  };
+  Action::None
+}
+
+pub fn handle_event(app: &App, tx: mpsc::UnboundedSender<Action>) -> tokio::task::JoinHandle<()> {
+  let tick_rate = std::time::Duration::from_millis(250);
+  tokio::spawn(async move {
+    loop {
+      let action = if crossterm::event::poll(tick_rate).unwrap() {
+        if let crossterm::event::Event::Key(key) = crossterm::event::read().unwrap() {
+          if key.kind == crossterm::event::KeyEventKind::Press {
+            match key.code {
+              crossterm::event::KeyCode::Char('j') => Action::ScheduleIncrement,
+              crossterm::event::KeyCode::Char('k') => Action::ScheduleDecrement,
+              crossterm::event::KeyCode::Char('q') => Action::Quit,
+              _ => Action::None,
+            }
+          } else {
+            Action::None
+          }
+        } else {
+          Action::None
+        }
+      } else {
+        Action::None
+      };
+      if let Err(_) = tx.send(action) {
+        break;
+      }
+    }
+  })
+}
+
+pub async fn run() -> Result<()> {
+  let mut t = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
+
+  let (action_tx, mut action_rx) = mpsc::unbounded_channel();
+
+  let mut app = App { counter: 0, should_quit: false, action_tx, ticker: 0, torrent_hashes: vec![] };
+
+  let (tx, rx) = unbounded::<ClientMessage>();
+
+  let torrent_client = TorrentClient::new();
+
+  let task = handle_event(&app, app.action_tx.clone());
+
+  loop {
+      app.torrent_hashes = torrent_client.torrents_mut.lock_mut().clone().into_keys().map(|info_hash| {
+        println!("{:?}", info_hash);
+        info_hash
+      }).collect::<Vec<_>>();
+
+      t.draw(|f| {
+        ui(f, &mut app);
+      })?;
+
+      if let Some(action) = action_rx.recv().await {
+        update(&mut app, action);
+      }
+
+      if app.should_quit {
+        break;
+      }
+      app.ticker += 1;
+  }
+
+  task.abort();
+
+  Ok(())
 }
